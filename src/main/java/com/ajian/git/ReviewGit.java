af38a0486d0265e39799cb108343d853c564be3c
ajian <atguigu@ajian.com>
2023-07-11 21:08:11 +0800

    hot-fix-just-commit

--- src/main/java/com/ajian/git/ReviewGit.java
+++ src/main/java/com/ajian/git/ReviewGit.java
@@ -3,5 +3,6 @@ package com.ajian.git;
 public class ReviewGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hot-fix just modify");
     }
 }
